f5ed92411a4006ae6a8936e8fb0747128ee69250
Apurva Saini <apps0602@gmail.com>
2020-12-18 11:25:40 +0530

    f

---
+++ models/script.js
@@ -0,0 +1,25 @@
+const video = document.getElementById('video')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo)
+
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {}},
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play',() => {
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks
+            ().withFaceExpressions()
+            console.log(detections)
+    },100)
+})
